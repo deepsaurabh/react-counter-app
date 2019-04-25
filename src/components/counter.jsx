872fbc216a2754dc0de2788c423a322dc7a05fb7
deep.singh <deep.singh@yatra.com>
2019-04-25 22:49:54 +0530

    changes related to lifting up props and having nav counter

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,22 +2,45 @@ import React, { Component } from "react";
 import List from "./list";
 
 class Counter extends Component {
-  state = {
-    count: 0,
-    tags: ["tag1", "tag2", "tag3"]
+  /*constructor() {
+    super();
+    console.log("constructor", this);
+    this.handkeIncrement = this.handkeIncrement.bind(this);
+  }*/
+
+  handleIncrement = () => {
+    this.setState({ value: this.state.value + 1 });
   };
+
   render() {
     return (
-      <React.Fragment>
-        <ul>
-          {this.state.tags.map(tag => (
-            <li key={tag}>{tag}</li>
-          ))}
-        </ul>
-
-        <List />
-      </React.Fragment>
+      <div>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button
+          onClick={() => this.props.onIncrement(this.props.counter)}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
+        <button
+          onClick={() => this.props.onDelete(this.props.counter.id)}
+          className="btn btn-danger btnsm m-2"
+        >
+          Delete
+        </button>
+      </div>
     );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.props.counter.value === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCount() {
+    const { value } = this.props.counter;
+    return value === 0 ? "Zero" : value;
   }
 }
 
